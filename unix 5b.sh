6d058a0bfb41318723f2bd4e2fd487313f33ff86
tejaswini h m <100576246+Tejaswinihm@users.noreply.github.com>
2022-04-06 16:42:27 +0530

    Update and rename 55b.sh to unix 5b.sh

--- unix 5b.sh
+++ unix 5b.sh
@@ -1,3 +1,8 @@
+# write a shell script to display the calender for the current month with current date replaced by * or ** 
+depending whether the date is one digit or two digit
+
+
+
 d=`date +%d`
 d=`expr $d + 0`
 l=`cal | tr -d "[\137][\010]" | grep -wn "$d" | head -c 1`
