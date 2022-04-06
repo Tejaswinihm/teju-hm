2d9470476033bc1ab37329e57439a6618081a48f
tejaswini h m <100576246+Tejaswinihm@users.noreply.github.com>
2022-04-06 16:34:48 +0530

    Update and rename prgm3.sh to unix 3a.sh

--- unix 3a.sh
+++ unix 3a.sh
@@ -1,6 +1,6 @@
 if [ -e $1 ]
 then
-set -- `ls -l $1`
+set -- `ls -ld $1`
 echo "permission are $1"
 echo "file link count: $2"
 echo "current user: $3"
@@ -9,6 +9,6 @@ echo "file size: $5"
 echo "modification is done on $6 $7 at $8"
 echo "file name: $9"
 else
-echo "file not exists"
+echo "file doesnt exit"
 fi
 
